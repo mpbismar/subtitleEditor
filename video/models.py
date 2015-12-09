4ea83249ea7fb2fb2ba44c5c6a6195a626bbf59a
Marc-Philipp Bismar <mpbismar@web.de>
2015-12-10 00:14:17 +0100

    userdatabse support

--- video/models.py
+++ video/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 
-class User(models.Model):
-    uid = models.PositiveIntegerField(primary_key=True, db_index=True, unique=True)
-    name = models.CharField(max_length=40)
-    n_apr = models.PositiveIntegerField(default=0)
-    n_cor = models.PositiveIntegerField(default=0)
-
 class Video(models.Model):
-    vid = models.PositiveIntegerField(primary_key=True, db_index=True, unique=True)
+    vid = models.AutoField(primary_key=True)
     name = models.CharField(max_length=40)
     lang = models.CharField(max_length=5)
     sub_langs = models.CharField(max_length=50)
     pub_date = models.DateTimeField(auto_now=True)
 
+class User(models.Model):
+    uid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40)
+    n_apr = models.PositiveIntegerField(default=0)
+    n_cor = models.PositiveIntegerField(default=0)
+
 class Sequence(models.Model):
-    sid = models.PositiveIntegerField(primary_key=True, db_index=True, unique=True)
+    sid = models.AutoField(primary_key=True)
     vid = models.ForeignKey(Video,to_field="vid")
     lang = models.CharField(max_length=5)
     content = models.CharField(max_length=150)
--- video/views.py
+++ video/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, render_to_response
-from .models import Video
+from .models import Video,User
 
 def index(request):
     latest_video_list = Video.objects.order_by('-pub_date')[:5]
@@ -14,6 +14,10 @@ def video(request, video_id):
 def login(request):
 
     # if this is a POST request we need to process the form data
-
+    if request.method == 'POST':
+        user=User(name=request.POST.get('username'))
+        user.save()
+        return render(request, 'video.html',{})
+    else :
         return render(request, 'login.html',{})
 
