e89c0df516794377b7be933e3ac8f555450f741f
Marc-Philipp Bismar <mpbismar@web.de>
2015-12-09 21:39:09 +0100

    corrected model specification

--- video/models.py
+++ video/models.py
@@ -3,27 +3,27 @@ from django.db import models
 class User(models.Model):
     uid = models.PositiveIntegerField(primary_key=True, db_index=True, unique=True)
     name = models.CharField(max_length=40)
-    n_votes = models.PositiveIntegerField(default=0)
-    n_corr = models.PositiveIntegerField(default=0)
-
-class Sequence(models.Model):
-    sid = models.PositiveIntegerField(primary_key=True, db_index=True, unique=True)
-    vid = models.PositiveIntegerField(serialize=False)
-    lang = models.CharField(max_length=5)
-    content = models.CharField(max_length=150)
-    start = models.PositiveIntegerField(serialize=False)
-    end = models.PositiveIntegerField(serialize=False)
+    n_apr = models.PositiveIntegerField(default=0)
+    n_cor = models.PositiveIntegerField(default=0)
 
 class Video(models.Model):
     vid = models.PositiveIntegerField(primary_key=True, db_index=True, unique=True)
     name = models.CharField(max_length=40)
     lang = models.CharField(max_length=5)
     sub_langs = models.CharField(max_length=50)
-    pub_date = models.DateTimeField(auto_now_add=True)
+    pub_date = models.DateTimeField(auto_now=True)
+
+class Sequence(models.Model):
+    sid = models.PositiveIntegerField(primary_key=True, db_index=True, unique=True)
+    vid = models.ForeignKey(Video,to_field="vid")
+    lang = models.CharField(max_length=5)
+    content = models.CharField(max_length=150)
+    start = models.PositiveIntegerField(serialize=False)
+    end = models.PositiveIntegerField(serialize=False)
 
 class Correction(models.Model):
-    vid = models.PositiveIntegerField(serialize=False)
-    sid = models.PositiveIntegerField(serialize=False)
+    vid = models.ForeignKey(Video,to_field="vid")
+    sid = models.ForeignKey(Sequence,to_field="sid")
     uids = models.CharField(max_length=500)
     new_content = models.CharField(max_length=150)
-    pub_date = models.DateTimeField(auto_now_add=True)
+    pub_date = models.DateTimeField(auto_now=True)
