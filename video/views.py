1caefe3258fb2041783296c0ee30a4d53320943c
Marc-Philipp Bismar <mpbismar@web.de>
2015-12-09 22:44:09 +0100

    started implementing dynamic video loading

--- video/views.py
+++ video/views.py
@@ -7,7 +7,6 @@ def index(request):
     return render(request, 'index.html', context)
 
 def video(request, video_id):
-    video_name_list = Video.objects.select_related(Video, "name").filter(Video, "vid"==video_id)
-    video_name = video_name_list[0]
-    context = {'video_name': video_name}
+    idvideo = Video(vid=video_id)
+    context = {'idvideo': idvideo}
     return render(request, 'video.html', context)
