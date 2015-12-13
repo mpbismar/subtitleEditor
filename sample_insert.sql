6eaf68a9c98e6151e8b11ad1a1b173a85b2d5e72
Marc-Philipp Bismar <mpbismar@web.de>
2015-12-13 22:38:34 +0100

    added sample_insert.sql

---
+++ sample_insert.sql
@@ -0,0 +1,17 @@
+use sub_db;
+DELETE FROM video_video WHERE vid < 0;
+DELETE FROM video_sequence WHERE sid < 0;
+INSERT INTO video_video (name,lang,sub_langs) VALUES ("flat-animation","en","en,de");
+INSERT INTO video_video (name,lang,sub_langs) VALUES ("Sample-Video-1","en","en,de");
+insert into video_sequence (vid_id,lang, content,start,end) values (1,"en", "english",100,2000);
+insert into video_sequence (vid_id,lang, content,start,end) values (1,"en", "Hey, bla, bla",2100,4000);
+insert into video_sequence (vid_id,lang, content,start,end) values (1,"en", "Hey, bla, bla",4100,6000);
+insert into video_sequence (vid_id,lang, content,start,end) values (1,"de", "deutsch",100,2000);
+insert into video_sequence (vid_id,lang, content,start,end) values (1,"de", "Hey, bla, bla",2100,4000);
+insert into video_sequence (vid_id,lang, content,start,end) values (1,"de", "Hey, bla, bla",4100,6000);
+insert into video_sequence (vid_id,lang, content,start,end) values (2,"en", "english",100,2000);
+insert into video_sequence (vid_id,lang, content,start,end) values (2,"en", "Hey, bla, bla",2100,4000);
+insert into video_sequence (vid_id,lang, content,start,end) values (2,"en", "Hey, bla, bla",4100,6000);
+insert into video_sequence (vid_id,lang, content,start,end) values (2,"de", "deutsch",100,2000);
+insert into video_sequence (vid_id,lang, content,start,end) values (2,"de", "Hey, bla, bla",2100,4000);
+insert into video_sequence (vid_id,lang, content,start,end) values (2,"de", "Hey, bla, bla",4100,6000);
